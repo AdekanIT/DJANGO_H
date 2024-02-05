347d8cc2557c7c07940f350a6447effb5781ed9a
AdekanIT <adekankain@gmail.com>
2024-02-05 09:07:31 +0500

    AlfaV

--- django_h/django_h/settings.py
+++ django_h/django_h/settings.py
@@ -75,9 +75,18 @@ WSGI_APPLICATION = 'django_h.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'news',
+        'USER': 'postgres',
+        'PORT': 5432,
+        'PASSWORD': '19772200$d',
+        'HOST': 'localhost'
+
     }
 }
 
